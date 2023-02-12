9bccf9732d82ee8ddf5df41752d14154be3c8eac
dundung <dundung@woowahan.com>
2023-02-13 02:23:52 +0900

    upload test static files

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require("@vue/cli-service");
-const path = require("path");
 module.exports = defineConfig({
-  outputDir: path.resolve("__dirname", "../../"),
+  publicPath: process.env.NODE_ENV === 'production'
+      ? '/typing-converter/'
+      : '/',
+  outputDir: "../docs",
   transpileDependencies: true,
 });
